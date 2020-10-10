05d3c96d542d88232e264cac7101c707a7a31330
tracer-ics2016 <tracer@njuics.org>
2020-10-10 13:31:09 +0000

    >  compile NEMU 10602121 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:31:09 up  1:30,  1 user,  load average: 2.28, 1.54, 1.32 293cbbb01526841ead4437d2d4c62afb79e73400

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -12,9 +12,9 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 	uint32_t offset = addr&(CACHE_BLOCK_SIZE-1);
 	uint8_t tmp[2*BURST_LEN];
 	if(offset+len>CACHE_BLOCK_SIZE) {
-		//int id2 = cache_read(addr+CACHE_BLOCK_SIZE-offset);	
+		int id2 = cache_read(addr+CACHE_BLOCK_SIZE-offset);	
 		//memcpy(tmp,cache[id].data+offset,CACHE_BLOCK_SIZE-offset);
-		//memcpy(tmp+CACHE_BLOCK_SIZE-offset,cache[id2].data,len-(CACHE_BLOCK_SIZE-offset));
+		memcpy(tmp+CACHE_BLOCK_SIZE-offset,cache[id2].data,len-(CACHE_BLOCK_SIZE-offset));
 	}
 	else {
 		memcpy(tmp,cache[id].data+offset,len);
