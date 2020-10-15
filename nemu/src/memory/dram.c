239c71b337e4bbd4585e9e05872b37dc886c6e04
tracer-ics2016 <tracer@njuics.org>
2020-10-15 07:47:56 +0000

    >  compile NEMU 10602121 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:47:56 up 3 days, 22:45,  1 user,  load average: 1.21, 0.99, 0.57 a0b8b89fceae30077cfe40d4cf050626eacc88d0

--- nemu/src/memory/dram.c
+++ nemu/src/memory/dram.c
@@ -76,20 +76,20 @@ void ddr3read(hwaddr_t addr, void *data) {
 
 	dram_addr temp;
 	temp.addr = addr & ~BURST_MASK;
-	uint32_t rank = temp.rank;
-	uint32_t bank = temp.bank;
-	uint32_t row = temp.row;
-	uint32_t col = temp.col;
+	//uint32_t rank = temp.rank;
+	//uint32_t bank = temp.bank;
+	//uint32_t row = temp.row;
+	//uint32_t col = temp.col;
 
-	if(!(rowbufs[rank][bank].valid && rowbufs[rank][bank].row_idx == row) ) {
+	//if(!(rowbufs[rank][bank].valid && rowbufs[rank][bank].row_idx == row) ) {
 		/* read a row into row buffer */
-		memcpy(rowbufs[rank][bank].buf, dram[rank][bank][row], NR_COL);
-		rowbufs[rank][bank].row_idx = row;
-		rowbufs[rank][bank].valid = true;
-	}
+	//	memcpy(rowbufs[rank][bank].buf, dram[rank][bank][row], NR_COL);
+	//	rowbufs[rank][bank].row_idx = row;
+	//	rowbufs[rank][bank].valid = true;
+	//}
 
 	/* burst read */
-	memcpy(data, rowbufs[rank][bank].buf + col, BURST_LEN);
+	//memcpy(data, rowbufs[rank][bank].buf + col, BURST_LEN);
 }
 
 
