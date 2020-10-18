2ecdfed8cb9e326e0571b689903a9bfab0fa0929
tracer-ics2016 <tracer@njuics.org>
2020-10-18 01:00:26 +0000

    >  compile NEMU 10602121 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:00:26 up 6 days, 15:57,  1 user,  load average: 0.93, 0.99, 0.69 3bae640f44a4c21d8dd9e183425e26a9f90ab74

--- nemu/src/memory/dram.c
+++ nemu/src/memory/dram.c
@@ -74,7 +74,25 @@ static void ddr3_read(hwaddr_t addr, void *data) {
 }
 
 void ddr3_read_public(hwaddr_t addr, void *data) {
-	ddr3_read(addr, data);
+	Assert(addr < HW_MEM_SIZE, "physical address %x is outside of the physical memory!", addr);
+
+	dram_addr temp;
+	temp.addr = addr & ~BURST_MASK;
+	uint32_t rank = temp.rank;
+	uint32_t bank = temp.bank;
+	uint32_t row = temp.row;
+	uint32_t col = temp.col;
+
+	if(!(rowbufs[rank][bank].valid && rowbufs[rank][bank].row_idx == row) ) {
+		/* read a row into row buffer */
+		memcpy(rowbufs[rank][bank].buf, dram[rank][bank][row], NR_COL);
+		rowbufs[rank][bank].row_idx = row;
+		rowbufs[rank][bank].valid = true;
+	}
+
+	/* burst read */
+	memcpy(data, rowbufs[rank][bank].buf + col, BURST_LEN);
+	//ddr3_read(addr, data);
 }
 
 static void ddr3_write(hwaddr_t addr, void *data, uint8_t *mask) {
