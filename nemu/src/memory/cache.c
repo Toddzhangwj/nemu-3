736b6d0bb41294bb28866727e1363600a589a87b
tracer-ics2016 <tracer@njuics.org>
2020-10-18 01:16:32 +0000

    >  compile NEMU 10602121 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:16:32 up 6 days, 16:13,  1 user,  load average: 1.08, 0.63, 0.56 e3aaa365edf00d8120437c26828168685ec30bb4

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -32,7 +32,7 @@ int readCache(hwaddr_t addr) {
 
 	j = readCache2(addr);
 	srand(i);
-	//i = CACHE_WAY_SIZE * set + rand() % CACHE_WAY_SIZE;//random
+	i = CACHE_WAY_SIZE * set + rand() % CACHE_WAY_SIZE;//random
 	//memcpy(cache[i].data, cache2[j].data, CACHE_BLOCK_SIZE);//CACHE2_BLOCK_SIZE=CACHE_BLOCK_SIZE
 
 /*
