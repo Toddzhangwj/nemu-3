5f32a32ef75b864c37e204c6271c71ecebb80003
tracer-ics2016 <tracer@njuics.org>
2020-10-18 02:53:13 +0000

    >  compile NEMU 3018216092 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:53:13 up 6 days, 17:50,  1 user,  load average: 1.74, 1.61, 1.43 4e47e7cecf7019eb8d772229481e16b3771796ac

--- lib-common/FLOAT.h
+++ lib-common/FLOAT.h
@@ -6,11 +6,11 @@
 typedef int FLOAT;
 
 static inline int F2int(FLOAT a) {
-	return (a & 0xffff0000) >> 16;
+	return (a >> 16);
 }
 
 static inline FLOAT int2F(int a) {
-	return a << 16;
+	return (a << 16);
 }
 
 static inline FLOAT F_mul_int(FLOAT a, int b) {
@@ -27,7 +27,6 @@ FLOAT F_div_F(FLOAT, FLOAT);
 FLOAT Fabs(FLOAT);
 FLOAT sqrt(FLOAT);
 FLOAT pow(FLOAT, FLOAT);
-float toFloat(FLOAT s);
 
 // used when calling printf/sprintf to format a FLOAT argument
 #define FLOAT_ARG(f) (long long)f
