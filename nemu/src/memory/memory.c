d5e11ba59bbf221f0d3a09ea3b9a3ff74fc7e805
tracer-ics2016 <tracer@njuics.org>
2020-10-20 11:59:48 +0000

    >  compile NEMU 3018216092 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:59:48 up 9 days,  2:57,  1 user,  load average: 1.03, 1.15, 1.00 a8aefb61c05c024ebadd1e7d5948ac192aa63873

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -24,8 +24,8 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 	}
 	
 	int zero = 0;
-	uint32_t tmp = unalign_rw(temp + zero, 4) & (~0u >> ((4 - len) << 3));
-	return tmp;
+	uint32_t ans = unalign_rw(temp + zero, 4) & (~0u >> ((4 - len) << 3));
+	return ans;
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {	//physical address
